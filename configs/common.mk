ba97c572fe040b607e40592a2030d9340c54ae7b
Alexander Martinz <eviscerationls@gmail.com>
2016-12-10 02:34:26 -0500

    cm: Fix Trusted Face
    Trusted Face depends on libprotobuf-cpp-full.
    Some devices already ship it (eg RIL needs it).
    
    Include it here to build it for every device.
    
    Link to OpenGapps issue:
    opengapps/opengapps#390
    
    Change-Id: Iffee0aa6218dfef45f5cd728a999fa1114c0dd57
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- configs/common.mk
+++ configs/common.mk
@@ -38,6 +38,10 @@ PRODUCT_PACKAGES += \
     Busybox \
     Launcher3
 
+# Import library to fix trusted face issues
+PRODUCT_PACKAGES += \
+libprotobuf-cpp-full \
+
 # Proprietary latinime libs needed for Keyboard swyping
 ifneq ($(filter saosp_shamu saosp_mako,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \

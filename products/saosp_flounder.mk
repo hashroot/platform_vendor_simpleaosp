6966bb4fc0fdcd6d3c1bb6f3c1cecffac08bd51d
Daniel Rowe <daniel.rowe@fathom13.com>
2017-07-08 18:08:22 +0800

    Update build fingerprint for flounder

--- products/saosp_flounder.mk
+++ products/saosp_flounder.mk
@@ -36,5 +36,5 @@ PRODUCT_MANUFACTURER := HTC
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N9F27C/3938480:user/release-keys \
-    PRIVATE_BUILD_DESC="flounder-user 7.1.1 N9F27C 3938480 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N9F27F/4072790:user/release-keys \
+    PRIVATE_BUILD_DESC="flounder-user 7.1.1 N9F27F 4072790 release-keys"
